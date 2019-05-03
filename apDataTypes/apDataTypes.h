f71347b68f36e9d6883eef8a07c6b505264fcfa0
Shane Burgess <acropup@users.noreply.github.com>
2019-05-03 12:14:48 -0700

    Update apDataTypes.h

--- apDataTypes/apDataTypes.h
+++ apDataTypes/apDataTypes.h
@@ -3,11 +3,11 @@
 
 // apDataTypes.h defines short names for the standard integer datatypes
 
-#typedef uint8_t  u8
-#typedef uint16_t u16
-#typedef uint32_t u32
-#typedef int8_t   s8
-#typedef int16_t  s16
-#typedef int32_t  s32
+typedef uint8_t  u8
+typedef uint16_t u16
+typedef uint32_t u32
+typedef int8_t   s8
+typedef int16_t  s16
+typedef int32_t  s32
 
 #endif

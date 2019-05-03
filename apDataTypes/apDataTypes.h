6a42e3d24ea85a8ba47860d6d2a45495cd450d23
Shane Burgess <acropup@users.noreply.github.com>
2019-05-03 12:23:11 -0700

    Update apDataTypes.h

--- apDataTypes/apDataTypes.h
+++ apDataTypes/apDataTypes.h
@@ -3,11 +3,20 @@
 
 // apDataTypes.h defines short names for the standard integer datatypes
 
-typedef uint8_t  u8
-typedef uint16_t u16
-typedef uint32_t u32
-typedef int8_t   s8
-typedef int16_t  s16
-typedef int32_t  s32
+#ifdef ARDUINO
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+#else
+typedef unsigned char   u8;
+typedef unsigned short  u16;
+typedef unsigned long   u32;
+typedef signed char     s8;
+typedef signed short    s16;
+typedef signed long     s32;
+#endif
 
 #endif
